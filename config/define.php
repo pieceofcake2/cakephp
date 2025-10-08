e38589c1c3a2a7432ec8233c2f49f65c9c0a14b1
TSURU <tsuyoshi.tsurushima@gmail.com>
2025-10-09 04:16:41 +0900

    refactor: Move all path constant definitions to config/define.php
    Consolidated all path constant definitions (APP, WEBROOT_DIR, TMP, LOGS, CACHE, CSS, JS, IMAGES, and URL constants) from bootstrap.php to config/define.php. This centralizes all path definitions in one location and simplifies bootstrap.php.

--- config/define.php
+++ config/define.php
@@ -4,6 +4,14 @@ if (!defined('DS')) {
     define('DS', DIRECTORY_SEPARATOR);
 }
 
+if (!defined('WEBROOT_DIR')) {
+    define('WEBROOT_DIR', 'webroot');
+}
+
+if (!defined('APP')) {
+    define('APP', ROOT . DS . APP_DIR . DS);
+}
+
 if (!defined('TMP')) {
     if (is_dir(ROOT . DS . 'tmp')) {
         define('TMP', ROOT . DS . 'tmp' . DS);
@@ -17,11 +25,13 @@ if (!defined('TMP')) {
 if (!defined('LOGS')) {
     if (is_dir(ROOT . DS . 'tmp')) {
         define('LOGS', ROOT . DS . 'logs' . DS);
-    } elseif (is_dir(APP . 'tmp')) {
-        define('LOGS', APP . 'tmp' . DS . 'logs' . DS);
     } else {
-        define('LOGS', CORE_ROOT . DS . 'logs' . DS);
+        define('LOGS', TMP . 'logs' . DS);
     }
+}
+
+if (!defined('CACHE')) {
+    define('CACHE', TMP . 'cache' . DS);
 }
 
 if (!defined('CONFIG')) {
@@ -38,4 +48,28 @@ if (!defined('APPLIBS')) {
 
 if (!defined('TESTS')) {
     define('TESTS', APP . 'Test' . DS);
+}
+
+if (!defined('CSS')) {
+    define('CSS', WWW_ROOT . 'css' . DS);
+}
+
+if (!defined('JS')) {
+    define('JS', WWW_ROOT . 'js' . DS);
+}
+
+if (!defined('IMAGES')) {
+    define('IMAGES', WWW_ROOT . 'img' . DS);
+}
+
+if (!defined('IMAGES_URL')) {
+    define('IMAGES_URL', 'img/');
+}
+
+if (!defined('CSS_URL')) {
+    define('CSS_URL', 'css/');
+}
+
+if (!defined('JS_URL')) {
+    define('JS_URL', 'js/');
 }
